d19dd2b731f9969e6f1678a8281affe261e02648
hal <haln_01@proton.me>
2025-06-27 16:32:56 -0700

    feat(block_api): get_block_range implemented

--- modules/api/services/block_api.go
+++ modules/api/services/block_api.go
@@ -10,13 +10,28 @@ type BlockAPI struct {
 }
 
 type GetBlockRangeArgs struct {
+	StartingBlockNum int `json:"starting_block_num"`
+	Count            int `json:"count"`
 }
 
 type GetBlockRangeReply struct {
+	Blocks []getBlockBlock `json:"blocks"`
 }
 
 func (BlockAPI) GetBlockRange(args *GetBlockRangeArgs, reply *GetBlockRangeReply) {
+	data, err := getMockData[GetBlockReply]("mockdata/block_api.get_block.mock.json")
+	if err != nil {
+		panic(err)
+	}
 
+	for i := 0; i <= args.Count; i++ {
+		blockIndex := fmt.Sprintf("%d", args.StartingBlockNum+i)
+		block, ok := data[blockIndex]
+		if !ok {
+			continue
+		}
+		reply.Blocks = append(reply.Blocks, block.Block)
+	}
 }
 
 type GetBlockArgs struct {
@@ -44,7 +59,6 @@ func (BlockAPI) GetBlock(args *GetBlockArgs, reply *GetBlockReply) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(args)
 
 	block, ok := data[fmt.Sprintf("%d", args.BlockNum)]
 	if !ok {
