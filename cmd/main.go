fb3bb88d816f0796e7204fcf950b139957d8073d
hal <haln_01@proton.me>
2025-06-30 11:32:05 -0700

    feat: intialize collections with common options

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,7 @@ import (
 	"mimic/modules/api"
 	"mimic/modules/db"
 	"mimic/modules/db/mimic"
-	"mimic/modules/db/mimic/blockdb"
 	"mimic/modules/db/mimic/condenserdb"
-	"mimic/modules/db/mimic/state"
 )
 
 func main() {
@@ -18,13 +16,13 @@ func main() {
 	mimicDb := mimic.New(db)
 	mimicDb.Init()
 
-	hiveBlocks := blockdb.New(mimicDb)
-	stateDb := state.New(mimicDb)
+	// hiveBlocks := blockdb.New(mimicDb)
+	// stateDb := state.New(mimicDb)
 	condenserDb := condenserdb.New(mimicDb)
 
 	plugins := []aggregate.Plugin{
-		hiveBlocks,
-		stateDb,
+		// hiveBlocks,
+		// stateDb,
 		condenserDb,
 	}
 
