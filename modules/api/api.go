7b63cc4238a1e194c531742c7e3e8887694c76df
Hal <haln_01@proton.me>
2025-07-02 12:37:47 -0700

    feat: added http logging

--- modules/api/api.go
+++ modules/api/api.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/httplog/v3"
 
 	"mimic/modules/api/services"
 	// ← v1 import path
@@ -52,6 +53,26 @@ func (s *APIServer) RegisterService(service services.ServiceHandler, name string
 
 func (s *APIServer) Init() {
 	router := chi.NewRouter()
+
+	loggerOpts := &httplog.Options{
+		// Level defines the verbosity of the request logs:
+		// slog.LevelDebug - log all responses (incl. OPTIONS)
+		// slog.LevelInfo  - log responses (excl. OPTIONS)
+		// slog.LevelWarn  - log 4xx and 5xx responses only (except for 429)
+		// slog.LevelError - log 5xx responses only
+		Level: slog.LevelInfo,
+
+		// Set log output to Elastic Common Schema (ECS) format.
+		Schema: httplog.SchemaECS,
+
+		// RecoverPanics recovers from panics occurring in the underlying HTTP handlers
+		// and middlewares. It returns HTTP 500 unless response status was already set.
+		//
+		// NOTE: Panics are logged as errors automatically, regardless of this setting.
+		RecoverPanics: true,
+	}
+
+	router.Use(httplog.RequestLogger(slog.Default(), loggerOpts))
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("go-mimic v1.0.0; Hive blockchain end to end simulation. To learn more, visit https://github.com/vsc-eco/go-mimic"))
