5d68126d0c508b19734a01993c59e96079604610
hal <haln_01@proton.me>
2025-07-25 14:47:34 -0700

    chore: added slices utils

--- lib/utils/slice.go
+++ lib/utils/slice.go
@@ -1,96 +1,26 @@
 package utils
 
-import (
-	"reflect"
+func TryMap[T, R any](a []T, mapper func(T) (R, error)) ([]R, error) {
+	var (
+		err error
+		out = make([]R, len(a))
+	)
 
-	"golang.org/x/exp/constraints"
-)
-
-func Reduce[T any, R any](a []T, reducer func(R, T) R, initial R) R {
-	res := initial
-	for _, v := range a {
-		res = reducer(res, v)
-	}
-	return res
-}
-
-type Number interface {
-	constraints.Float | constraints.Integer
-}
-
-func Sum[T Number](a []T) T {
-	return Reduce(a, func(acc T, v T) T {
-		return acc + v
-	}, 0)
-}
-
-func Map[T any, R any](a []T, mapper func(T) R) []R {
-	res := make([]R, len(a))
 	for i, v := range a {
-		res[i] = mapper(v)
-	}
-	return res
-}
-
-func Remove[T any](a []T, v T) []T {
-	index := IndexOf(a, v)
-	if index == -1 {
-		return a
-	}
-
-	return Concat(a[:index], a[index+1:])
-}
-
-func IndexOf[T any](a []T, v T) int {
-	for i, val := range a {
-		// stupid compiler doesn't understand T is always comparable to T
-		if reflect.DeepEqual(val, v) {
-			return i
+		out[i], err = mapper(v)
+		if err != nil {
+			return nil, err
 		}
 	}
-	return -1
+
+	return out, nil
 }
 
-func Concat[T any](a []T, b []T) []T {
-	res := make([]T, len(a)+len(b))
-	copy(res, a)
-	copy(res[len(a):], b)
-	return res
-}
-
-// In place merge sort
-func MergeSort[T constraints.Ordered](a []T) {
-	l := len(a)
-	if l <= 1 {
-		return
-	}
-	m := l / 2
-	left := a[:m]
-	right := a[m:]
-	MergeSort(left)
-	MergeSort(right)
-	merge(left, right, m, l)
-}
-
-// [0, 2, 4] + [1, 3, 5] => [0, 1, 2] [3, 4, 5]
-func merge[T constraints.Ordered](left []T, right []T, m int, l int) {
-	lp := 0
-	lmax := m
-
-	rp := 0
-	rmax := l - m
-
-	for lp < lmax && rp < rmax {
-		lval := left[lp]
-		rval := right[rp]
-
-		if lval > rval {
-			right[rp] = lval
-			left[lp] = rval
-
-			merge(right[:rp+1], right[rp+1:], 1, rmax)
+func TryForEach[T any](a []T, fn func(T) error) error {
+	for _, v := range a {
+		if err := fn(v); err != nil {
+			return err
 		}
-
-		lp += 1
 	}
+	return nil
 }
