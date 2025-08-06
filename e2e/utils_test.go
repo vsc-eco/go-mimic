0278ed85c9f91d48d063e12aa4607bbafbfe2a3f
hal <haln_01@proton.me>
2025-08-05 17:58:21 -0700

    test: e2e testing with http handlers `/`, `/health`

---
+++ e2e/utils_test.go
@@ -0,0 +1,11 @@
+package e2e_test
+
+import (
+	"fmt"
+	"strings"
+)
+
+func makeGoMimicEndpoint(port uint16, path ...string) string {
+	base := fmt.Sprintf("http://0.0.0.0:%d", port)
+	return base + "/" + strings.Join(path, "/")
+}
