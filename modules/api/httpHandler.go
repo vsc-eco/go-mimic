d7f657f3fa71144b56acb074e4482b256c18eb65
hal <haln_01@proton.me>
2025-08-05 17:57:55 -0700

    fix: exported root message

--- modules/api/httpHandler.go
+++ modules/api/httpHandler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-const rootMsg = "go-mimic v1.0.0; Hive blockchain end to end simulation. To learn more, visit https://github.com/vsc-eco/go-mimic"
+const RootMsg = "go-mimic v1.0.0; Hive blockchain end to end simulation. To learn more, visit https://github.com/vsc-eco/go-mimic"
 
 type httpHandler struct {
 	logger *slog.Logger
@@ -13,7 +13,7 @@ type httpHandler struct {
 
 func (h *httpHandler) root(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(rootMsg)); err != nil {
+	if _, err := w.Write([]byte(RootMsg)); err != nil {
 		h.logger.Error("failed to write response", "err", err)
 	}
 }
